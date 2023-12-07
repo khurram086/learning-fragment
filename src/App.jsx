c80bc433c391304710641e08b777a84e2050a5e8
khurram086 <khurram.mehdi786@gmail.com>
2023-12-07 19:33:59 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css'
 
 function App() {
-  
   let foodItems=["Dal","Green Vegetable","Roti","Salad","Milk"];
+  // let foodItems=[];
 
+  if (foodItems.length===0){
+    return <h3>I am Still Hungry.</h3>;
+  }
+  // let emptyMessage=foodItems.length===0 ? <h3>I am very hungry.</h3> : <h3>Array has no Value.</h3>;
   return (
   <>
       <h1>Healthy Food</h1>
-  <ul class="list-group">
+  <ul className="list-group">
     {foodItems.map((item) => (
-    <li class="list-group-item">{item}</li>
+    <li key={item} className="list-group-item">{item}</li>
     ))}
   
 </ul>
